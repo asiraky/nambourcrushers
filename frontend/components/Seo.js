808dcaf6f04bf74b2007c122d0bb7706d7c4ae88
Zoran Perin <perinzoran@gmail.com>
2021-02-13 10:07:01 +0100

    FIX: favicons

--- frontend/components/Seo.js
+++ frontend/components/Seo.js
@@ -43,10 +43,10 @@ const Seo = ({ seo }) => {
             {fullSeo.article && <meta property="og:type" content="article" />}
             <meta name="twitter:card" content="summary_large_image" />
 
-			<link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
-			<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
-			<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
-			<link rel="manifest" href="favicons/site.webmanifest" />
+			<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+			<link rel="manifest" href="/favicons/site.webmanifest" />
 
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
 			<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600&display=swap" rel="stylesheet" />
